84186f858859e760277239954aa9bdad6faaa330
mgdhibi@integrationobjects.com <Life12358(hamma)>
2022-08-31 16:00:31 +0100

    refactor: Add abstract class IUpdateItemsQuality

---
+++ GildedRose/Models/IUpdateItemsQuality.cs
@@ -0,0 +1,12 @@
+﻿using GildedRoseKata.Interfaces;
+using GildedRoseKata.Models;
+
+namespace GildedRoseKata.Models
+{
+    public abstract class IUpdateItemsQuality : Item, IItemQuality
+    {
+        public int MinimumQuality { get; set; } = 0;
+        public int MaximumQuality { get; set; } = 50;
+        public abstract void UpdateQuality();
+    }
+}
