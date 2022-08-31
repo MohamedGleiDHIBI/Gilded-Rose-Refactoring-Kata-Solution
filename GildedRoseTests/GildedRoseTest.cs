a82857b70602e2bcda320a6bc6fb87201eeb0c13
mgdhibi@integrationobjects.com <Life12358(hamma)>
2022-08-31 14:40:36 +0100

    test: Add QualityDropTo0_BackstagePassesAfterConcert

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -55,5 +55,11 @@ namespace GildedRoseTests
             var Item = GenrateItems("Backstage passes to a TAFKAL80ETC concert", 5, 35);
             Assert.AreEqual(38, Item.Quality);
         }
+        [Test]
+        public void GildedRose_QualityDropTo0_BackstagePassesAfterConcert()
+        {
+            var Item = GenrateItems("Backstage passes to a TAFKAL80ETC concert", 0, 35);
+            Assert.AreEqual(0, Item.Quality);
+        }
     }
 }
