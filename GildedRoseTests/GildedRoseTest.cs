eae9c6ceca64ba80dc6a5a2bf595412f3f9b94ea
mgdhibi@integrationobjects.com <Life12358(hamma)>
2022-08-31 14:27:14 +0100

    test: Quality Always Positive

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -7,12 +7,12 @@ namespace GildedRoseTests
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void GildedRose_QualityAlwaysPositive()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("foo", Items[0].Name);
+            Assert.AreEqual(0, Items[0].Quality);
         }
     }
 }
