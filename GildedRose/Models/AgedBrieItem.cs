8e4f8304ee6446ab1f18349d35e8dbe88707632b
mgdhibi@integrationobjects.com <Life12358(hamma)>
2022-08-31 16:02:19 +0100

    refactor: Add AgedBrieItem Class And implement UpdateQuality

---
+++ GildedRose/Models/AgedBrieItem.cs
@@ -0,0 +1,18 @@
+﻿namespace GildedRoseKata.Models
+{
+    internal class AgedBrieItem : IUpdateItemsQuality
+    {
+        public override void UpdateQuality()
+        {
+            SellIn -= 1;// SellIn decrease ALways for AgedBrie
+
+            if (SellIn >= 0)
+                Quality++;// If SellIn Positive Increase Quality By 1
+            else
+                Quality += 2;//If SellIn negative Increase Quality By 2
+
+            if (Quality > 50)
+                Quality = MaximumQuality;  // Quality ALways Lower Than 50 
+        }
+    }
+}
