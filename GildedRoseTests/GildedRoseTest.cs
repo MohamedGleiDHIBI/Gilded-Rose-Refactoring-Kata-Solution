49f6449914198eca8298bc508a13fc303b14c3e8
mgdhibi@integrationobjects.com <Life12358(hamma)>
2022-08-31 14:11:06 +0100

    Build: Replace XUnit bu NUnit and Add coverlet.msbuild

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -1,18 +1,18 @@
-﻿using Xunit;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GildedRoseKata;
+using NUnit.Framework;
 
 namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
-        [Fact]
+        [Test]
         public void foo()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
         }
     }
 }
